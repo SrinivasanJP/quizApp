adeb4631b922148d7f0f9c8fad6a9372b2c6a52d
Siva kumar <sivakumar091020035@gmail.com>
2024-03-03 11:58:05 +0530

    navigation completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 import Navigation from './components/Navigation'
-
+import CreateQuizPage from './components/CreateQuizPage'
+const renderComponent = (fragment)=>{
+  switch(fragment){
+    case ("createQuiz"):
+      return <CreateQuizPage />
+  }
+}
 const App = () => {
+  const [fragment, setFragment] = useState("home")
   return (
-    <Navigation/>
+    <div>
+      <Navigation setFragment={setFragment}/>
+      {renderComponent(fragment)}
+    </div>
+    
   )
 }
 
